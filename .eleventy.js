4f00313227314593d692ec29025beeb3adfafe3f
Brendan Conard <117928315+Brendanthatman@users.noreply.github.com>
2025-01-29 00:44:31 +0000

    video changes

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,5 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  //eleventyConfig.addPassthroughCopy("**/*.png"); take any png we put in the repo and move it to _site after running the npx command 
 };
